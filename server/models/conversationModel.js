1f0cb4e8385312b40de7b93d947419f2c8915b89
Michael Sarkisian <msarkisian1996@gmail.com>
2022-09-16 17:33:31 -0400

    mongoose models

---
+++ server/models/conversationModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const conversationSchema = new mongoose.Schema({
+  message: [
+    {
+      author: { type: String, required: true },
+      content: { type: String, required: true },
+      createdAt: { type: Date, default: Date.now },
+      translations: [
+        {
+          language: { type: String, required: true },
+          content: { type: String, required: true },
+        },
+      ],
+    },
+  ],
+});
+
+const Conversation = mongoose.model('Conversation', conversationSchema);
+
+module.exports = Conversation;
