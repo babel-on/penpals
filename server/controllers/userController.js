5e85a007b2102520097348e6bb5a6f0d72bf8798
jchan <james.bz.chan@gmail.com>
2022-09-17 12:39:47 -0700

    Merge branch James into dev

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,7 +3,35 @@ const userController = {};
 const bcrypt = require('bcryptjs');
 
 const SALT_WORK_FACTOR = 10;
-
+const langCode = {
+  BG: 'Bulgarian',
+  CS: 'Czech',
+  DA: 'Danish',
+  DE: 'German',
+  EL: 'Greek',
+  EN: 'English',
+  ES: 'Spanish',
+  ET: 'Estonian',
+  FI: 'Finnish',
+  FR: 'French',
+  HU: 'Hungarian',
+  ID: 'Indonesian',
+  IT: 'Italian',
+  JA: 'Japanese',
+  LT: 'Lithuanian',
+  LV: 'Latvian',
+  NL: 'Dutch',
+  PL: 'Polish',
+  PT: 'Portuguese',
+  RO: 'Romanian',
+  RU: 'Russian',
+  SK: 'Slovak',
+  SL: 'Slovenian',
+  SV: 'Swedish',
+  TR: 'Turkish',
+  UK: 'Ukrainian',
+  ZH: 'Chinese',
+};
 // create user
 userController.createUser = async (req, res, next) => {
   if (!req.body.username)
@@ -24,6 +52,13 @@ userController.createUser = async (req, res, next) => {
       status: 400,
       message: 'Language required',
     });
+  if(!langCode.hasOwnProperty(req.body.language))
+    return next({
+      log:null,
+      status:400,
+      message: 'Language not supported'
+    });
+    
   try {
     const passwordHash = await bcrypt.hash(req.body.password, SALT_WORK_FACTOR);
     const user = await User.create({
