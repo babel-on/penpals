e0c7895130e6632602423782e8245afc90664161
jchan <james.bz.chan@gmail.com>
2022-09-16 17:27:03 -0700

    Merge branch James into dev

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+
+const salt = 10;
+const bcrypt = require('bcryptjs');
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
@@ -7,6 +11,11 @@ const userSchema = new mongoose.Schema({
   conversations: [
     { type: mongoose.Schema.Types.ObjectId, ref: 'Conversation' },
   ],
+});
+
+userSchema.pre('save', function (next) {
+  this.password = bcrypt.hashSync(this.password, salt);
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
