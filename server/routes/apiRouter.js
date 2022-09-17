4b2b50404b5a09d5c6838ea760e108300591325b
jchan <james.bz.chan@gmail.com>
2022-09-16 17:37:15 -0700

    Merge branch James into dev

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
+const app = require('../server');
 const router = express.Router();
 // requrie controller
+const userController = require('../controllers/userController.js');
 
-//
+//handle login
+router.post('/login', userController.verifyUser, (req, res) => {
+  res.status(200).json(res.locals.userData);
+});
+
+
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -48,3 +48,4 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
