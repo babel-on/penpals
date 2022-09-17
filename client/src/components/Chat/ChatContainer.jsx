09d0f358d0fd1c508ec01452eafe233793b5a155
Adrian <adrianwreczek@gmail.com>
2022-09-17 11:37:19 -0500

    added final react componenets to chat page skeleton

--- client/src/components/Chat/ChatContainer.jsx
+++ client/src/components/Chat/ChatContainer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import MessageBox from './MessageBox';
+import MessageCreator from './MessageCreator';
+
 const ChatContainer = props => {
   return (
     <div className='chatContainer'>
       <h1>ChatContainer</h1>
+      <MessageBox />
+      <MessageCreator />
     </div>
   );
 };
