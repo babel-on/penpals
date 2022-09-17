c6b1dea278b50fa0741c85c245aa318c9d7909c4
Michael Sarkisian <msarkisian1996@gmail.com>
2022-09-16 20:23:23 -0400

    translation function working

--- server/utls/fetchTranslation.js
+++ server/utls/fetchTranslation.js
@@ -3,8 +3,12 @@ const deepl = require('deepl-node');
 
 dotenv.config();
 const { DEEPL_KEY } = process.env;
-const translator = new deepl.translator(DEEPL_KEY);
+const translator = new deepl.Translator(DEEPL_KEY);
 
 const fetchTranslation = (lang, content) => {
   return translator.translateText(content, null, lang);
 };
+
+fetchTranslation('JA', 'I really hope this works this time around!').then(
+  (res) => console.log(res)
+);
