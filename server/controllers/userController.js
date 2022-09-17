8779c18bec28719254cfe9f46cafe63a89fdde50
jchan <james.bz.chan@gmail.com>
2022-09-17 09:05:51 -0700

    Merge branch James into dev

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,48 +6,60 @@ const SALT_WORK_FACTOR = 10;
 
 // create user
 userController.createUser = (req, res, next) => {
-  bcrypt.hash(req.body.password, SALT_WORK_FACTOR)
+  bcrypt
+    .hash(req.body.password, SALT_WORK_FACTOR)
     .then((hashedPass) => {
       User.create({
         username: req.body.username,
         passwordHash: hashedPass,
-        language: req.body.language
+        language: req.body.language,
       })
-        .then(createdUser => {
+        .then((createdUser) => {
           res.locals.createdUser = createdUser;
           next();
         })
-        .catch(err => next({
-          log: 'userController.createUser ERROR',
-          status: 500,
-          message: {err: 'userController.createUser ERROR: Username already exists'}}))
+        .catch((err) =>
+          next({
+            log: 'userController.createUser ERROR',
+            status: 500,
+            message: {
+              err: 'userController.createUser ERROR: Username already exists',
+            },
+          })
+        );
     })
-    .catch(err => next({
-      log: 'userController.createUser ERROR',
-      status: 500,
-      message: {err: 'userController.createUser ERROR: Password has failed'}
-    }));
+    .catch((err) =>
+      next({
+        log: 'userController.createUser ERROR',
+        status: 500,
+        message: {
+          err: 'userController.createUser ERROR: Password has failed',
+        },
+      })
+    );
 };
 
 // verify user
 userController.verifyUser = async (req, res, next) => {
   try {
-    const user = await User.findOne({username: req.body.username});
+    const user = await User.findOne({ username: req.body.username });
     const passwordOk = await bcrypt.compare(req.body.password, user.password);
-    if (passwordOk){
+    if (passwordOk) {
       res.locals.user = user;
       return next();
     }
     return next({
       log: 'userController.verifyUser ERROR',
       status: 500,
-      message: {err: 'userController.verifyUser ERROR: Invalid Username or password'}
-    })
+      message: {
+        err: 'userController.verifyUser ERROR: Invalid Username or password',
+      },
+    });
   } catch {
     return next({
       log: 'userController.verifyUser ERROR',
       status: 500,
-      message: {err: 'userController.verifyUser ERROR: Error verifying user'}
+      message: { err: 'userController.verifyUser ERROR: Error verifying user' },
     });
   }
 };
