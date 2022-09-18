6dabd8bab0473f4e3b9ef77b1563da7536436e64
Jigar Patel <jigarxp@gmail.com>
2022-09-18 19:03:26 -0400

    removed hard coded language and added draggable components

--- client/src/pages/ChatPage/Container.jsx
+++ client/src/pages/ChatPage/Container.jsx
@@ -5,6 +5,7 @@ import ConvoContainer from '../../components/Chat/ConvoContainer/ConvoContainer.
 import RandomConvo from '../../components/Chat/RandomConvo/RandomConvoContainer.jsx';
 import UserContext from '../../context/UserContext';
 import './Container.scss';
+import interact from 'interactjs';
 
 const Container = () => {
   // // individual conversations
@@ -15,6 +16,22 @@ const Container = () => {
   //   const convo = await fetch();
   //   setConversation();
   // };
+  const position = { x: 0, y: 0 };
+
+  interact('.convoContainer').draggable({
+    listeners: {
+      start(event) {
+        console.log(event.type, event.target);
+      },
+      move(event) {
+        position.x += event.dx;
+        position.y += event.dy;
+
+        event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+      },
+    },
+  });
+
   return (
     <div className="container">
       <Navbar />
