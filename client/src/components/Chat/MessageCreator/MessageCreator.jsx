3dce62c1f1bdeac8d41fa45b8fae3a0dc40594b7
Adrian <adrianwreczek@gmail.com>
2022-09-18 16:11:25 -0500

    added pseudocode for messagecreator fetch

--- client/src/components/Chat/MessageCreator/MessageCreator.jsx
+++ client/src/components/Chat/MessageCreator/MessageCreator.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
 const MessageCreator = () => {
-  // // to store new user input message in state
   // const [newMessage, setNewMessage] = useState('');
 
+  //state needed: current conversation id, current users
+  //make post request to /api/conversation/:id (conversation id)
+  //req.body should have: 
+  /*
+      const message = {
+      author: res.locals.user.username, -> should already be taken care of by jwtcontroller
+      content: req.body.content, --> what we are submitting
+      // we also add the message as-is as a valid translation for the user's current language
+      translations: {
+        [res.locals.user.language]: req.body.content,
+      },
+    };
+  */
+  //after post, doesn't have to append the new message to message box directly, but should signal to it to re-fetch conversation
+  
+  const clickHandler = (e) => {
+    
+  }
+
   //needs event handler onclick for button to submit form message to route
+  //after posting message to conversation should also update state of message box
   return (
     <form className="messageCreator">
       <input type="text"></input>
       {/* onchange for input field-> on change {(newValue) => setNewMessage(newValue.target.value)}? */}
 
-      <button type="submit">Send</button>
+      <button onClick={clickHandler} className='sendButton'>Send</button>
     </form>
   );
 };
