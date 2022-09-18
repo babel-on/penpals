a4e1bd4e9e08a8adb37a8033bd9ef3b97d208129
Adrian <adrianwreczek@gmail.com>
2022-09-18 18:09:56 -0500

    added preliminary message box context use

--- client/src/components/Chat/MessageBox/MessageBox.jsx
+++ client/src/components/Chat/MessageBox/MessageBox.jsx
@@ -1,26 +1,31 @@
 import React, { useContext, useEffect } from 'react';
-import { useFetch } from '../../../hooks/useFetch';
 import UserContext from '../../../context/UserContext';
 
 import IncomingMessages from '../../Chat/MessageBox/Messages/IncomingMessages';
-import OutgoingMessages from '../../Chat/MessageBox/Messages/OutgoingMessages'
+import OutgoingMessages from '../../Chat/MessageBox/Messages/OutgoingMessages';
 
 const MessageBox = () => {
 
-  const {currentConversation, handleCurrentConversation} = useContext(UserContext);
-
+  const {currentConversation, user} = useContext(UserContext);
+  const messages = [];
   useEffect(() => {
-    fetch('/api/conversation/')
+    fetch(`/api/conversation/${currentConversation}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-      })
-  })
+        for (const message of data){
+          if (message.author === user){
+            messages.push(<OutgoingMessages message={message.content} />);
+          } else if (message.author !== user){
+            messages.push(<IncomingMessages message={message.content} />);
+          }
+        }
+      });
+  });
 
   return (
     <div className="messageBox">
       <h2>Message Box</h2>
-
+      {messages}
     </div>
   );
 };
