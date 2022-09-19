1363bc908fa6ba040d50961f62d967ca91b96424
Jigar Patel <jigarxp@gmail.com>
2022-09-18 20:07:23 -0400

    fixed currenconversation fetching and useeffect

--- client/src/components/Chat/RandomConvo/RandomConvoContainer.jsx
+++ client/src/components/Chat/RandomConvo/RandomConvoContainer.jsx
@@ -3,27 +3,23 @@ import RandomUser from './RandomUser/RandomUser';
 import UserContext from '../../../context/UserContext';
 
 const RandomConvo = () => {
-  const {randomList, handleRandomList} = useContext(UserContext);
+  const { randomList, handleRandomList } = useContext(UserContext);
 
-  useEffect(() => {    
+  useEffect(() => {
     fetch('/api/getusers')
       .then((res) => res.json())
       .then((data) => {
         handleRandomList(
           data.map((el) => <RandomUser key={el._id} user={el} />)
         );
-      })
-      .then(() => console.log(randomList));
+      });
   }, []);
 
-  
-
   return (
-    <div className='randomList'>
+    <div className="randomList">
       <h1>Random Users</h1>
       {randomList}
     </div>
   );
-
 };
 export default RandomConvo;
