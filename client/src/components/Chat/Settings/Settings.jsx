c64bd7f886cbd0f733f8884564f646c40ac8ca24
Michael Sarkisian <msarkisian1996@gmail.com>
2022-09-19 14:26:49 -0400

    fix refreshability

--- client/src/components/Chat/Settings/Settings.jsx
+++ client/src/components/Chat/Settings/Settings.jsx
@@ -13,6 +13,7 @@ const Settings = () => {
   const handleClose = (e) => {
     e.target.parentElement.classList.remove('show_side_bar');
   };
+  if (!user) return;
 
   return (
     <div className="settings">
@@ -27,7 +28,7 @@ const Settings = () => {
       >
         <path
           onClick={handleOpen}
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
         />
       </svg>
